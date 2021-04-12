295479c4e10077abfb755765b6558ee487980bc9
Hassan Ahmad <ahmad12p@uwindsor.ca>
2021-04-11 20:38:56 -0400

    Updated the text channel and removed all other map embeds to 1 only

--- src/bot.js
+++ src/bot.js
@@ -36,7 +36,8 @@ client.on("guildMemberRemove", (member) => {
 
 client.on("message", async (message) => {
   //this is so we who sent the message, not the BOT
-  if (message.author.bot) return;
+  // if (message.author.bot) return;
+  if (message.author.client) return;
 
   if (message.content === "gay") {
     message.channel.send("We know Wargasm-OG- is GAY boi");
@@ -49,7 +50,7 @@ client.on("message", async (message) => {
       .substring(PREFIX.length)
       .split(/\s+/);
 
-    //Knife Command (Basically clip a coin)
+    //Knife Command (Basically flip a coin)
     if (CMD_NAME === "veto") {
       const maps = [
         "Mirage",
@@ -123,120 +124,28 @@ client.on("messageReactionAdd", async (reaction, user) => {
   if (reaction.emoji.name == "❌") reaction.message.delete();
   if (reaction.emoji.name == "✅") {
     setTimeout(() => message.delete(), 2000);
-    await client.channels.fetch("818174578039586836");
-    let channel = client.channels.cache.get("818174578039586836");
+    await client.channels.fetch("830954302658445362");
+    let channel = client.channels.cache.get("830954302658445362");
     channel.messages
       .fetch({ limit: 1 })
       .then((messages) => {
         let lastMessage = messages.first();
 
         if (lastMessage.author.client) {
-          if (lastMessage.content === "Mirage") {
-            const embed = {
-              title: `Map is \`${lastMessage}\``,
-              description: `Click the link below to join the Pandemonium server\n\nConnect: steam://connect/208.167.251.244:27035\nPassword: **chaos**\n\nCopy in console:\n**connect 208.167.251.244:27035;password chaos**`,
-              color: 7584788,
-              thumbnail: {
-                url: "https://i.imgur.com/LspvXhH.jpg",
-              },
-              timestamp: new Date(),
-              footer: {
-                text: "Please join the server in 5min",
-                icon_url: "https://i.imgur.com/vHIoeL4.png",
-              },
-            };
-            message.channel.send({ embed });
-          } else if (lastMessage.content === "Dust 2") {
-            const mapDust2 = {
-              title: `Map is \`${lastMessage}\``,
-              description: `Click the link below to join the Pandemonium server\n\nConnect: steam://connect/208.167.251.244:27035\nPassword: **chaos**\n\nCopy in console:\n**connect 208.167.251.244:27035;password chaos**`,
-              color: 7584788,
-              thumbnail: {
-                url: "https://i.imgur.com/h8YLmAM.jpg",
-              },
-              timestamp: new Date(),
-              footer: {
-                text: "Please join the server in 5min",
-                icon_url: "https://i.imgur.com/vHIoeL4.png",
-              },
-            };
-            message.channel.send({ embed: mapDust2 });
-          } else if (lastMessage.content === "Inferno") {
-            const mapInferno = {
-              title: `Map is \`${lastMessage}\``,
-              description: `Click the link below to join the Pandemonium server\n\nConnect: steam://connect/208.167.251.244:27035\nPassword: **chaos**\n\nCopy in console:\n**connect 208.167.251.244:27035;password chaos**`,
-              color: 7584788,
-              thumbnail: {
-                url: "https://i.imgur.com/cpH8vnd.jpg",
-              },
-              timestamp: new Date(),
-              footer: {
-                text: "Please join the server in 5min",
-                icon_url: "https://i.imgur.com/vHIoeL4.png",
-              },
-            };
-            message.channel.send({ embed: mapInferno });
-          } else if (lastMessage.content === "Nuke") {
-            const mapNuke = {
-              title: `Map is \`${lastMessage}\``,
-              description: `Click the link below to join the Pandemonium server\n\nConnect: steam://connect/208.167.251.244:27035\nPassword: **chaos**\n\nCopy in console:\n**connect 208.167.251.244:27035;password chaos**`,
-              color: 7584788,
-              thumbnail: {
-                url: "https://i.imgur.com/8ERmYwv.jpg",
-              },
-              timestamp: new Date(),
-              footer: {
-                text: "Please join the server in 5min",
-                icon_url: "https://i.imgur.com/vHIoeL4.png",
-              },
-            };
-            message.channel.send({ embed: mapNuke });
-          } else if (lastMessage.content === "Overpass") {
-            const mapOverpass = {
-              title: `Map is \`${lastMessage}\``,
-              description: `Click the link below to join the Pandemonium server\n\nConnect: steam://connect/208.167.251.244:27035\nPassword: **chaos**\n\nCopy in console:\n**connect 208.167.251.244:27035;password chaos**`,
-              color: 7584788,
-              thumbnail: {
-                url: "https://i.imgur.com/ZhP3hKo.jpg",
-              },
-              timestamp: new Date(),
-              footer: {
-                text: "Please join the server in 5min",
-                icon_url: "https://i.imgur.com/vHIoeL4.png",
-              },
-            };
-            message.channel.send({ embed: mapOverpass });
-          } else if (lastMessage.content === "Train") {
-            const mapTrain = {
-              title: `Map is \`${lastMessage}\``,
-              description: `Click the link below to join the Pandemonium server\n\nConnect: steam://connect/208.167.251.244:27035\nPassword: **chaos**\n\nCopy in console:\n**connect 208.167.251.244:27035;password chaos**`,
-              color: 7584788,
-              thumbnail: {
-                url: "https://i.imgur.com/PoYG4bk.jpg",
-              },
-              timestamp: new Date(),
-              footer: {
-                text: "Please join the server in 5min",
-                icon_url: "https://i.imgur.com/vHIoeL4.png",
-              },
-            };
-            message.channel.send({ embed: mapTrain });
-          } else if (lastMessage.content === "Vertigo") {
-            const mapVertigo = {
-              title: `Map is \`${lastMessage}\``,
-              description: `Click the link below to join the Pandemonium server\n\nConnect: steam://connect/208.167.251.244:27035\nPassword: **chaos**\n\nCopy in console:\n**connect 208.167.251.244:27035;password chaos**`,
-              color: 7584788,
-              thumbnail: {
-                url: "https://i.imgur.com/pQrBzZk.jpg",
-              },
-              timestamp: new Date(),
-              footer: {
-                text: "Please join the server in 5min",
-                icon_url: "https://i.imgur.com/vHIoeL4.png",
-              },
-            };
-            message.channel.send({ embed: mapVertigo });
-          }
+          const embed = {
+            title: `Map is \`${lastMessage}\``,
+            description: `Click the link below to join the Pandemonium server\n\nConnect: steam://connect/208.167.251.244:27035\nPassword: **chaos**\n\nCopy in console:\n**connect 208.167.251.244:27035;password chaos**`,
+            color: 7584788,
+            thumbnail: {
+              url: "https://i.imgur.com/BFtfLbo.jpg[/img]",
+            },
+            timestamp: new Date(),
+            footer: {
+              text: "Please join the server in 5min",
+              icon_url: "https://i.imgur.com/vHIoeL4.png",
+            },
+          };
+          message.channel.send({ embed });
         }
       })
       .catch(console.error);
