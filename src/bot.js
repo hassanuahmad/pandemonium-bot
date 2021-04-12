8e63b0ba4ff0571052c82b53f2f83a75db052d11
Hassan Ahmad <ahmad12p@uwindsor.ca>
2021-04-11 23:47:43 -0400

    Added !clear Command

--- src/bot.js
+++ src/bot.js
@@ -117,6 +117,58 @@ client.on("message", async (message) => {
   }
 });
 
+/* CODE COPIED SOF - START */
+client.on("message", (message) => {
+  if (!message.content.startsWith(PREFIX) || message.author.bot) return;
+
+  const args = message.content
+    .toLowerCase()
+    .slice(PREFIX.length)
+    .trim()
+    .split(/\s+/);
+  const [command, input] = args;
+
+  if (command === "clear") {
+    if (!message.member.hasPermission("MANAGE_MESSAGES")) {
+      return message.channel.send("You don't have the permissions!");
+    }
+
+    if (isNaN(input)) {
+      return message.channel
+        .send("Enter the amount messages that you would like to delete")
+        .then((sent) => {
+          setTimeout(() => {
+            sent.delete();
+          }, 2500);
+        });
+    }
+
+    if (Number(input) < 0) {
+      return message.channel.send("Enter a positive number").then((sent) => {
+        setTimeout(() => {
+          sent.delete();
+        }, 2500);
+      });
+    }
+
+    // add an extra to delete the current message too
+    const amount = Number(input) > 100 ? 101 : Number(input) + 1;
+
+    message.channel.bulkDelete(amount, true).then((_message) => {
+      message.channel
+        // do you want to include the current message here?
+        // if not it should be ${_message.size - 1}
+        .send(`BOT deleted \`${_message.size}\` messages :broom:`)
+        .then((sent) => {
+          setTimeout(() => {
+            sent.delete();
+          }, 2500);
+        });
+    });
+  }
+});
+/* CODE COPIED SOF - END */
+
 client.on("messageReactionAdd", async (reaction, user) => {
   const message = reaction.message;
   if (message.author.id == user.id) return;
