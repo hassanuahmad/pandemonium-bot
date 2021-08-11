3507162fbc1d9affb408047a26fa3bfb0847bd90
hassanuahmad <hassanusaidahmad@gmail.com>
2021-08-10 20:27:44 -0400

    Testing Join/Leave Events

--- events/join.js
+++ events/join.js
@@ -3,13 +3,13 @@ const { MessageEmbed } = require("discord.js");
 module.exports = {
     name: "guildMemberAdd",
     once: false,
-    async execute(client, members) {
+    async execute(client) {
         let guild = client.guilds.cache.get("714305139539968013");
         const Channel = client.channels.cache.get("872000479406723074");
         const embed = new MessageEmbed()
             .setColor("GREEN")
             .setTitle("New Homebread")
-            .setDescription(`**${members.displayName}**, Welcome to Pandemonium!`);
+            .setDescription(`Welcome to Pandemonium!`);
         await Channel.send({ embeds: [embed] });
     },
 };
--- events/leave.js
+++ events/leave.js
@@ -3,15 +3,13 @@ const { MessageEmbed } = require("discord.js");
 module.exports = {
     name: "guildMemberRemove",
     once: false,
-    async execute(client, members) {
+    async execute(client) {
         let guild = client.guilds.cache.get("714305139539968013");
         const Channel = client.channels.cache.get("872000479406723074");
         const embed = new MessageEmbed()
             .setColor("RED")
             .setTitle("Homebread Gone :(")
-            .setDescription(
-                `**${members.displayName}** has left Pandemonium :(`
-            );
+            .setDescription(`left Pandemonium :(`);
         await Channel.send({ embeds: [embed] });
     },
 };
