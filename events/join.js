25b48ac7c8ae0ebc6d4e8e8e27f51321cdca7b11
hassanuahmad <hassanusaidahmad@gmail.com>
2021-08-10 20:32:55 -0400

    Updated Join Leave

--- events/join.js
+++ events/join.js
@@ -3,13 +3,15 @@ const { MessageEmbed } = require("discord.js");
 module.exports = {
     name: "guildMemberAdd",
     once: false,
-    async execute(client) {
+    async execute(client, members) {
         let guild = client.guilds.cache.get("714305139539968013");
         const Channel = client.channels.cache.get("872000479406723074");
         const embed = new MessageEmbed()
             .setColor("GREEN")
             .setTitle("New Homebread")
-            .setDescription(`Welcome to Pandemonium!`);
+            .setDescription(
+                `**${members.displayName}**, Welcome to Pandemonium!`
+            );
         await Channel.send({ embeds: [embed] });
     },
 };
--- events/leave.js
+++ events/leave.js
@@ -3,13 +3,15 @@ const { MessageEmbed } = require("discord.js");
 module.exports = {
     name: "guildMemberRemove",
     once: false,
-    async execute(client) {
+    async execute(client, members) {
         let guild = client.guilds.cache.get("714305139539968013");
         const Channel = client.channels.cache.get("872000479406723074");
         const embed = new MessageEmbed()
             .setColor("RED")
             .setTitle("Homebread Gone :(")
-            .setDescription(`left Pandemonium :(`);
+            .setDescription(
+                `**${members.displayName}** has left Pandemonium :(`
+            );
         await Channel.send({ embeds: [embed] });
     },
 };
