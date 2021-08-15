882651b84d0e5e9df9b022774d95a94dd7cc8926
hassanuahmad <hassanusaidahmad@gmail.com>
2021-08-14 20:28:21 -0400

    Updated the server;js file and added permissions for the clear.js file

--- commands/clear.js
+++ commands/clear.js
@@ -1,25 +1,34 @@
 module.exports = {
     name: "clear",
-    description: "Command that clears messeges from the server",
     async execute(message, args) {
-        if (!args[0])
-            return message.channel.send(
-                "Please enter the amount of messages that you want to clear!"
-            );
-        if (isNaN(args[0]))
-            return message.channel.send("Please enter a real number!");
-        if (args[0] > 100)
-            return message.channel.send(
-                "You can't delete more than 100 messeges!"
-            );
-        if (args[0] < 1)
-            return message.channel.send("You must clear atleast 1 message!");
+        if (
+            message.member.roles.cache.has("778424134044352513") ||
+            message.member.roles.cache.has("845758498863316993")
+        ) {
+            if (!args[0])
+                return message.channel.send(
+                    "Please enter the amount of messages that you want to clear!"
+                );
+            if (isNaN(args[0]))
+                return message.channel.send("Please enter a real number!");
+            if (args[0] > 100)
+                return message.channel.send(
+                    "You can't delete more than 100 messeges!"
+                );
+            if (args[0] < 1)
+                return message.channel.send(
+                    "You must clear atleast 1 message!"
+                );
 
-        await message.channel.messages
-            .fetch({ limit: args[0] })
-            .then((messages) => {
-                message.channel.bulkDelete(messages);
-            });
-        // message.channel.send("Deleted " + args[0] + " messages 🧹");
+            await message.channel.messages
+                .fetch({ limit: args[0] })
+                .then((messages) => {
+                    message.channel.bulkDelete(messages);
+                });
+        } else {
+            message.channel.send(
+                `You **DON'T** have permissions to use this command!`
+            );
+        }
     },
 };
--- commands/server.js
+++ commands/server.js
@@ -3,7 +3,6 @@ const { prefix, server_password } = require("../config.json");
 
 module.exports = {
     name: "server",
-    description: "Information about our CS:GO Server",
     execute(message) {
         setTimeout(() => message.delete(), 2000);
         const embed = new MessageEmbed()
@@ -11,9 +10,9 @@ module.exports = {
             .setTitle("Pandemonium Servers")
             .setDescription(
                 `
-                **Public Server**\n\`pandepublic1.noob.club:27576\`\n
-                **Retakes Server**\n\`panderetakes1.noob.club:28408\`\n
-                **Private Server**\n\`pandeprivate.noob.club:27870;password ${server_password}\`\n              
+                **5v5 Server**\n\`connect pandepublic1.noob.club:27576\`\n
+                **Retakes Server**\n\`connect panderetakes1.noob.club:28408\`\n
+                **Private 10man Server**\n\`connect pandeprivate.noob.club:27870;password ${server_password}\`\n              
                 `
             )
             .setThumbnail("https://i.imgur.com/vHIoeL4.png");
